9c95af77f044421b4460f1f3974ad1751f6af771
pushpkumar <pkumar69@myseneca.ca>
2022-07-23 23:06:07 +0500

    final

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <footer class="page-footer">
         <div class="container">
-            <div class="links"><a href="cv.php">About me</a><a href="hire-me.php">Contact me</a><a href="projects-grid-cards.php">Projects</a></div>
+            <div class="links"><a href="cv.html">About me</a><a href="hire-me.html">Contact me</a><a href="projects-grid-cards.html">Projects</a></div>
             <div class="social-icons"><a href="https://www.facebook.com/mazanlabeeb"><i class="icon ion-social-facebook"></i></a><a href="https://www.instagram.com/mazanlabeeb"><i class="icon ion-social-instagram-outline"></i></a><a href="https://wa.me/+923061695230/?text=Contact Mazan Labeeb"><i class="icon ion-social-whatsapp-outline"></i></a></div>
         </div>
     </footer>
--- include/header.php
+++ include/header.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
-        <div class="container"><a class="navbar-brand logo" href="index.php">Mazan Labeeb</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarNav"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container"><a class="navbar-brand logo" href="index.html">Mazan Labeeb</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navbarNav"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="projects-grid-cards.php">Projects</a></li>
-                    <li class="nav-item"><a class="nav-link" href="cv.php">CV</a></li>
-                    <li class="nav-item"><a class="nav-link" href="hire-me.php">Hire me</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="projects-grid-cards.html">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="cv.html">CV</a></li>
+                    <li class="nav-item"><a class="nav-link" href="hire-me.html">Hire me</a></li>
                 </ul>
             </div>
         </div>
